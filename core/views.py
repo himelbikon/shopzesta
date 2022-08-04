656c621da480b2bb6c9f93f1d08c84c3c266e211
himelbikon <skhimelhaque@gmail.com>
2022-08-04 16:47:46 +0600

    carousel image added

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from product.models import Product
+from .models import *
 
 
 def index(request):
     data = {
         'title': 'Home | Shopzesta',
-        'products': Product.objects.all()[0:8]
+        'products': Product.objects.all()[0:8],
+        'carousel': Carousel.objects.all(),
     }
     return render(request, 'core/index.html', data)
